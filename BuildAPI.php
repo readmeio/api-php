042fdcca93bedeada29653bf6c1693c682d0883e
Bazlur <mailtobazlur@gamil.com>
2017-08-02 20:18:20 +0600

    git ignore added, build api class created

---
+++ BuildAPI.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 8/2/2017
+ * Time: 8:03 PM
+ */
+
+class BuildAPI {
+    const API_PATH = 'https://api.readme.build/v0/services/';
+    const HTTP_OPEN_TIMEOUT = 5;
+
+    function config($api_key, $password = '')
+    {
+        $api_key = $api_key;
+        $password = $password;
+    }
+}
